c7c4468a0cb1c3ca9b30b2a2917211e0282bb068
gongweichuan@gmail.com <gongweichuan@gmail.com@934a62da-e7e6-11dd-bce9-bf72c0239d3a>
2011-03-08 16:06:14 +0000

    设置java-6-sun和已有ant和项目内置ant的冲突。

--- code/client/corepower/boost/build.sh
+++ code/client/corepower/boost/build.sh
@@ -2,8 +2,8 @@
 
 #linux下的命令行脚本 UTF－8编码
 
-chmod u+x ../tools/bin/antRun
-chmod u+x ../tools/bin/ant
+chmod u+x  ../../tools/bin/antRun
+chmod u+x  ../../tools/bin/ant
 
 # ----- Verify and Set Required Environment Variables -------------------------
 
@@ -12,6 +12,7 @@ if [ "$TERM" = "cygwin" ] ; then
 else
   S=':'
 fi
+#需要设置JAVA_HOME和消除PATH的影响
 
 # ----- Ignore system CLASSPATH variable
 OLD_CLASSPATH="$CLASSPATH"
@@ -21,12 +22,13 @@ export CLASSPATH
 
 # ----- Use Ant shipped with Lenya. Ignore installed in the system Ant
 OLD_ANT_HOME="$ANT_HOME"
-ANT_HOME=tools
+ANT_HOME=../../tools
 OLD_ANT_OPTS="$ANT_OPTS"
-ANT_OPTS="-Xms32M -Xmx512M -Djava.endorsed.dirs=lib/endorsed"
+ANT_OPTS="-Xms32M -Xmx512M "
 export ANT_HOME ANT_OPTS
-
-"$ANT_HOME/bin/ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
+#echo "$ANT_HOME/bin/ant"
+#"$ANT_HOME/bin/./ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
+ant -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
 
 # ----- Restore ANT_HOME and ANT_OPTS
 ANT_HOME="$OLD_ANT_HOME"
