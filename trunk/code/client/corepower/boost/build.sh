f50b23eeb39b5af8591edf49d64cf4621c48ecfb
GongWeichuan@gmail.com <GongWeichuan@gmail.com@934a62da-e7e6-11dd-bce9-bf72c0239d3a>
2011-03-08 07:23:33 +0000

    增加运行SHELL脚本。 缺少JAVA_HOME的配置、需要特殊的JDK版本和对应的包。

--- trunk/code/client/corepower/boost/build.sh
+++ trunk/code/client/corepower/boost/build.sh
@@ -1,1 +1,41 @@
+#!/bin/sh
+
 #linux下的命令行脚本 UTF－8编码
+
+chmod u+x ../tools/bin/antRun
+chmod u+x ../tools/bin/ant
+
+# ----- Verify and Set Required Environment Variables -------------------------
+
+if [ "$TERM" = "cygwin" ] ; then
+  S=';'
+else
+  S=':'
+fi
+
+# ----- Ignore system CLASSPATH variable
+OLD_CLASSPATH="$CLASSPATH"
+unset CLASSPATH
+CLASSPATH="`echo lib/*.jar | tr ' ' $S`"
+export CLASSPATH
+
+# ----- Use Ant shipped with Lenya. Ignore installed in the system Ant
+OLD_ANT_HOME="$ANT_HOME"
+ANT_HOME=tools
+OLD_ANT_OPTS="$ANT_OPTS"
+ANT_OPTS="-Xms32M -Xmx512M -Djava.endorsed.dirs=lib/endorsed"
+export ANT_HOME ANT_OPTS
+
+"$ANT_HOME/bin/ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
+
+# ----- Restore ANT_HOME and ANT_OPTS
+ANT_HOME="$OLD_ANT_HOME"
+ANT_OPTS="$OLD_ANT_OPTS"
+export ANT_HOME ANT_OPTS
+unset OLD_ANT_HOME
+unset OLD_ANT_OPTS
+
+# ----- Restore CLASSPATH
+CLASSPATH="$OLD_CLASSPATH"
+export CLASSPATH
+unset OLD_CLASSPATH
