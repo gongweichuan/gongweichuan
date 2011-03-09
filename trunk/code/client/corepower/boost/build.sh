faeeeb269337e4918c6cdb5af8588c352754592d
gongweichuan@gmail.com <gongweichuan@gmail.com@934a62da-e7e6-11dd-bce9-bf72c0239d3a>
2011-03-09 15:54:40 +0000

    增加全局配置文件

--- trunk/code/client/corepower/boost/build.sh
+++ trunk/code/client/corepower/boost/build.sh
@@ -14,10 +14,23 @@ else
 fi
 #需要设置JAVA_HOME和消除PATH的影响
 
+#JAVA_HOME
+OLD_JAVA_HOME="$JAVA_HOME"
+unset JAVA_HOME
+JAVA_HOME="/usr/lib/jvm/java-6-sun"
+export JAVA_HOME
+
+#PATH
+OLD_PATH="$PATH"
+unset PATH
+PATH="$JAVA_HOME/bin"
+export PATH
+
 # ----- Ignore system CLASSPATH variable
 OLD_CLASSPATH="$CLASSPATH"
 unset CLASSPATH
-CLASSPATH="`echo lib/*.jar | tr ' ' $S`"
+#CLASSPATH="`echo lib/*.jar | tr ' ' $S`"
+#CLASSPATH="`echo lib/*.jar`"
 export CLASSPATH
 
 # ----- Use Ant shipped with Lenya. Ignore installed in the system Ant
@@ -26,9 +39,8 @@ ANT_HOME=../../tools
 OLD_ANT_OPTS="$ANT_OPTS"
 ANT_OPTS="-Xms32M -Xmx512M "
 export ANT_HOME ANT_OPTS
-#echo "$ANT_HOME/bin/ant"
-#"$ANT_HOME/bin/./ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
-ant -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
+echo "$ANT_HOME/bin/ant"
+"$ANT_HOME/bin/ant" -logger org.apache.tools.ant.NoBannerLogger -emacs  $@
 
 # ----- Restore ANT_HOME and ANT_OPTS
 ANT_HOME="$OLD_ANT_HOME"
@@ -41,3 +53,13 @@ unset OLD_ANT_OPTS
 CLASSPATH="$OLD_CLASSPATH"
 export CLASSPATH
 unset OLD_CLASSPATH
+
+#Restore PATH
+PATH="$OLD_PATH"
+export PATH
+unset OLD_PATH
+
+#Restore JAVA_HOME
+JAVA_HOME="$OLD_JAVA_HOME"
+export JAVA_HOME
+unset OLD_JAVA_HOME
