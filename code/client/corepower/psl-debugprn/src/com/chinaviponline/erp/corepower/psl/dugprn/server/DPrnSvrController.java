c15f7a6ca4e5980b09ad5f1066c2d525465d86c0
gongweichuan@gmail.com <gongweichuan@gmail.com@934a62da-e7e6-11dd-bce9-bf72c0239d3a>
2011-09-09 07:10:20 +0000

    基础插件提交psl-debugprn

---
+++ code/client/corepower/psl-debugprn/src/com/chinaviponline/erp/corepower/psl/dugprn/server/DPrnSvrController.java
@@ -0,0 +1,201 @@
+/**
+ * 
+ */
+package com.chinaviponline.erp.corepower.psl.dugprn.server;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+import com.chinaviponline.erp.corepower.api.BaseMBeanSupport;
+
+/**
+ * <p>文件名称：DPrnSvrController.java</p>
+ * <p>文件描述：</p>
+ * <p>版权所有： 版权所有(C)2007-2017</p>
+ * <p>公    司： 与龙共舞独立工作室</p>
+ * <p>内容摘要： </p>
+ * <p>其他说明： </p>
+ * <p>完成日期：2008-6-5</p>
+ * <p>修改记录1：</p>
+ * <pre>
+ *  修改日期：    版本号：    修改人：    修改内容：
+ * </pre>
+ * <p>修改记录2：</p>
+ *
+ * @version 1.0
+ * @author 龚为川
+ * @email  gongweichuan(AT)gmail.com
+ */
+public class DPrnSvrController extends BaseMBeanSupport implements DPrnSvrControllerMBean
+{
+
+    public String addJMSAppender(String s, String s1, String s2)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String addSocketAppender(String s, Integer integer, String s1) throws Exception
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getConfigureFile()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getConsoleLogLevel()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getFileLogLevel()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Hashtable getLoggerTree()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Hashtable getServerInfo()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getServerName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getSuperHost()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void removeAllNetAppender()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void removeJMSAppender(String s)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void removeSocketAppender(String s) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public String removeSocketAppender(String s, Integer integer, String s1)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setAppenderThreshold(String s, Integer integer) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setAppendersThreshold(HashSet hashset, HashMap hashmap) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setConfigureFile(String s)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setFilter(String s, Integer integer)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setServerName(String s)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setSuperHost(String s)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void startLog4jServer(String s) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void stopLog4jServer(String s) throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public String getName()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int getState()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public String getStateString()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void create() throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void destroy()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void start() throws Exception
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void stop()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
